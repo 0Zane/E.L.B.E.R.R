a172a7773d0567ac384dc766039cd45e9abdb022
Zane <labs.zane@gmail.com>
2026-07-14 20:06:05 +0200

    Updating pins & removing nfc features

---
+++ esp32-firmware/include/pins.h
@@ -0,0 +1,19 @@
+//I2C BUS 
+#define SCL 9
+#define SDA 8
+
+//SPI BUS
+#define MOSI 11
+#define MISO 13
+#define SCK 12
+#define NRF_CS 10
+
+//UART WITH RASPBERRY PI
+#define TX 17
+#define RX 18
+
+//LED
+#define NRFLED 1
+#define OSKULL 2
+#define CSKULL 3
+#define NFSKULL 4
