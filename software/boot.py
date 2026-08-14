c4d21b89b8e66b25e6643624d31795ad80d7fdac
Zane <labs.zane@gmail.com>
2026-08-14 22:42:53 +0200

    Replaceing command name to play sound

--- software/boot.py
+++ software/boot.py
@@ -4,4 +4,4 @@ import subprocess
 script_dir = os.path.dirname(os.path.abspath(__file__))
 audio_file = os.path.join(script_dir, "../booting.WAV")
 
-subprocess.run(["aplay", audio_file])
+subprocess.run(["paplay", audio_file])
