094067bea36e7c12cc215289f0b00e60ee9c020e
Zane <labs.zane@gmail.com>
2026-08-14 23:05:43 +0200

    Testing delay time for audio playing

--- software/boot.py
+++ software/boot.py
@@ -1,8 +1,11 @@
 import os
 import subprocess
+import time
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 audio_file = os.path.join(script_dir, "../booting.WAV")
 
-subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "100%"])
+time.sleep(5)
+
+subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", "120%"])
 subprocess.run(["paplay", audio_file])
