e83916d1ca5af21c1722cf9633a251a637aea700
Zane <labs.zane@gmail.com>
2026-08-14 02:48:55 +0200

    Allowing user input for AI prompt

--- software/main.py
+++ software/main.py
@@ -1,5 +1,6 @@
 from ai import LLM
 
 if __name__ == "__main__":
-    test_prompt = "Hello, what is your name and how are you ?"
-    print(LLM(test_prompt))
+    while True:
+        user_prompt = input("Speak to AI: \n")
+        print(LLM(user_prompt))
